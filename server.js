2238c86c1d50ad2d594c66a9c561d1ff1bb69b4e
prateekwonderswhy <58238077+prateekwonderswhy@users.noreply.github.com>
2021-03-18 17:46:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-var http = require("http");
+// var http = require("http");
 var https = require("https");
 var express = require("express");
 var bodyParser = require('body-parser')
@@ -7,13 +7,13 @@ var {router}=require('./routes')
 
 var privateKey=fs.readFileSync("key.pem")
 var certificate=fs.readFileSync("cert.pem")
-var credentials = { key: privateKey, cert: certificate, passphrase: "1234" };
+var credentials = { key: privateKey, cert: certificate, passphrase: process.env.CRED_PASSPHRASE };
 var app = express();
-var httpServer = http.createServer(app);
+// var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 app.use(bodyParser.json({extended:true}))
 app.use(router)
 app.use(function(req,res){res.sendStatus(404);})
-httpServer.listen(8080, (p, h) => console.log(`HTTP  Server listening `));
-httpsServer.listen(8443, (p, h) => console.log(`HTTPS Server listening`));
+// httpServer.listen(8080, (p, h) => console.log(`HTTP  Server listening `));
+httpsServer.listen(8443,'0.0.0.0', (p, h) => console.log(`HTTPS Server listening at 0.0.0.0:8443`));
