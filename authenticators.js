b1c516f2071778220d9013e67944a2fc5e4a3e59
prateekwonderswhy <58238077+prateekwonderswhy@users.noreply.github.com>
2021-03-22 17:21:19 +0530

    Update authenticators.js

--- authenticators.js
+++ authenticators.js
@@ -5,7 +5,7 @@ function GithubAuthenticator(header/** 'X-Hub-Signature-256' */,secret,algo='sha
 
         // check if header value is not very large, 71 is the total length including the preceding "sha256=" text
         var shaHeader=request.get(header)
-        if ((!shaHeader) ||shaHeader.length>=71)return false
+        if ((!shaHeader) ||shaHeader.length>71)return false
         else return crypto.createHmac(algo, secret)
             .update(JSON.stringify(request.body))
             .digest("hex")===shaHeader.slice(7)
