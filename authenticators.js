46dbc42f9e8365221af8210629d439acae286ae0
prateekwonderswhy <58238077+prateekwonderswhy@users.noreply.github.com>
2021-03-22 16:44:04 +0530

    + length limit sha256 header value
    , + using slice rather than using split('=')

--- authenticators.js
+++ authenticators.js
@@ -3,10 +3,12 @@ function GithubAuthenticator(header/** 'X-Hub-Signature-256' */,secret,algo='sha
     const crypto=require('crypto');
     return function(request){
 
-        //request.body is expected to be hold a value
-        return crypto.createHmac(algo, secret)
-        .update(JSON.stringify(request.body))
-        .digest("hex")===request.get(header)?.split("=").pop()
+        // check if header value is not very large, 71 is the total length including the preceding "sha256=" text
+        var shaHeader=request.get(header)
+        if ((!shaHeader) ||shaHeader.length>=71)return false
+        else return crypto.createHmac(algo, secret)
+            .update(JSON.stringify(request.body))
+            .digest("hex")===shaHeader.slice(7)
     }
 }
 
